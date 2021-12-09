eacff1744ec785071559d877e9578e7d6c3f3589
Joseph-112 <44301921+Joseph-112@users.noreply.github.com>
2021-12-08 19:13:59 -0500

    Proyecto final

---
+++ js/firebase.js
@@ -0,0 +1,84 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAQJ_LXcT4v8aW9E4PAWQETmjyhSk5NASA",
+  authDomain: "clase-firebase-ccdbe.firebaseapp.com",
+  projectId: "clase-firebase-ccdbe",
+  storageBucket: "clase-firebase-ccdbe.appspot.com",
+  messagingSenderId: "190611273653",
+  appId: "1:190611273653:web:47a8f25ed000171f974bfc",
+  measurementId: "G-GY4CR10GEQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+window.salir = function salir() {
+      const auth = getAuth();
+      signOut(auth).then(() =>{
+            document.location.href = "/";
+      }).catch((err) =>{
+          alert("Se produce error al cerrar la sesion");
+          console.log(err);
+      });
+      
+}
+
+
+export function verAutenticacion(){
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) =>{
+        if(user) {
+          /*Al iniciar sesion
+          console.log("Inicio sesion");  
+          console.log(user);*/
+
+          console.log(user);
+          if(document.getElementById("divRedes"))
+            document.getElementById("divRedes").style.visibility = "hidden";
+
+          if(document.getElementById("divInicioSesion"))        
+            document.getElementById("divInicioSesion").style.visibility = "hidden";          
+
+
+          if(user.photoURL != null)
+            document.getElementById("imgFotoUsuario").src= user.photoURL;
+          else
+            document.getElementById("imgFotoUsuario").src= "asset/img/nouser.jpg";
+
+          if(user.displayName != null)
+              document.getElementById("lblNombreUsuario").innerHTML = user.displayName;
+          else if (user.email != null)    
+              document.getElementById("lblNombreUsuario").innerHTML = user.email;
+          else if (user.reloadUserInfo.screenName != null)    
+              document.getElementById("lblNombreUsuario").innerHTML = user.reloadUserInfo.screenName;              
+          else
+              document.getElementById("lblNombreUsuario").innerHTML = ""; 
+
+          document.getElementById("barraMenuId").style.visibility = "visible";
+          document.getElementById("divDatosUsu").style.visibility = "visible";
+
+
+
+        } else {
+         /** 
+          * Al cerrar sesion
+          * 
+         */
+          document.getElementById("barraMenuId").style.visibility = "hidden";
+          document.getElementById("divDatosUsu").style.visibility = "hidden";
+
+          if(document.getElementById("divRedes"))
+            document.getElementById("divRedes").style.visibility = "visible";
+
+          if(document.getElementById("divInicioSesion"))  
+            document.getElementById("divInicioSesion").style.visibility = "visible";
+        }
+    });
+}
